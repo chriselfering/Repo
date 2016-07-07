3b0ac12ff93194a8d04269c6f064135830549cfd
chriselfering <chriselfering@gmail.com>
2016-07-07 12:03:41 -0600

    loopPractice1

---
+++ week2/loopPractice1.js
@@ -0,0 +1,22 @@
+// for (var i = 0; i < 5; i++)
+//   console.log(i);
+//
+// for (var i = 0; i < 501; i += 100)
+//   console.log(i);
+
+// for (var i = 1; i < 65; i = i + i)
+//   console.log(i);
+//
+// for (var i = 0; i < 11; i+=2)
+//   console.log(i)
+
+// for (var i = 3; i < 16; i+=3)
+//   console.log(i)
+
+// for (var i = 9;  i > -1; i--)
+//   console.log(i)
+
+for (var i = 0; i < 4; i++) 
+  for (var x = 0; x < 3; x++)
+    console.log(i);
+    console.log(x);
